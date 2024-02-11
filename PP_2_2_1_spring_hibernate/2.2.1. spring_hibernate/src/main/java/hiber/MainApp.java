fbf13cb2adb37fc4136416e01c28e4c62c6ddb87
DmitriyBelousovd <94719275+DmitriyBelousovd@users.noreply.github.com>
2024-02-11 18:41:42 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,40 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru",
+              new Car("Toyota", 70));
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru",
+              new Car("BMW", 3));
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru",
+              new Car("Volvo", 60));
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+
+      User userByCar = userService.getUserByCar(new Car("BMW", 3));
+      System.out.println(userByCar);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println(user);
+      }
+
+      context.close();
+   }
+}
---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,45 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   private final SessionFactory sessionFactory;
+
+   @Autowired
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   public List<User> listUsers() {
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User", User.class);
+      return query.getResultList();
+   }
+
+   @Override
+   public User getUserByCar(Car car) {
+      String hql = """
+                FROM User u
+                WHERE u.car.model = :carModel
+                AND u.car.series = :carSeries
+                """;
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+      query.setParameter("carModel", car.getModel());
+      query.setParameter("carSeries", car.getSeries());
+
+      return query.getSingleResult();
+   }
+}
